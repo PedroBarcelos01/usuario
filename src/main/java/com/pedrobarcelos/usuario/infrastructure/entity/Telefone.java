c2df8fca9ae2d97505d3dc6f9d2872bbb5141ac7
Pedro Barcelos <pedromiguel1203@hotmail.com>
2026-06-26 19:01:39 -0300

    adicionado método que salva dados do usuário

--- src/main/java/com/pedrobarcelos/usuario/infrastructure/entity/Telefone.java
+++ src/main/java/com/pedrobarcelos/usuario/infrastructure/entity/Telefone.java
@@ -1,10 +1,7 @@
 package com.pedrobarcelos.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @Getter
@@ -13,7 +10,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "telefone")
+@Builder
 public class Telefone {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,4 +22,5 @@ public class Telefone {
     private String ddd;
     @Column(name = "ddi", length = 2)
     private String ddi;
+
 }
